c5dbcd25575fd5c70abc1321bd09bc0648f1ae6c
Gerardo Chaves <ggchaves@gmail.com>
2019-11-22 15:17:52 -0600

    Better documentation and cleanup removing obsolete functions and variables

--- compute.py
+++ compute.py
@@ -16,74 +16,9 @@ import shutil
 import json, requests
 import time
 from datetime import datetime
-from config import _FILTER_TIME, NETWORK_ID, MERAKI_API_KEY, _SESSION_TIME, timeWindow, rssiThreshold
+from config import NETWORK_ID, MERAKI_API_KEY
 
-# opens cmxdata and formats to show only relevant information
-# if time difference is within session time, the times are considered within the same session, if they are out, they are considered 2 different sessions
-def getCMXHours(data):
-    arrayCount = 0
-    newData = []
-    for x in data:
-        timeEntries = len(x['timestamps'])
-        if timeEntries > 1:
-            firstSeen =0
-            time2=timeEntries-1
-            time1=timeEntries-2
-            startFlag=0
-            timeCount=0
-            while time1 >= 0:
-                if (int(x['timestamps'][time1]) - int(x['timestamps'][time2])) < _SESSION_TIME and startFlag == 0 and firstSeen == 0:
-                    # print(str(x['timestamps'][time1]) + ' - ' + str(x['timestamps'][time2]) + ' = ' + str(int(x['timestamps'][time1]) - int(x['timestamps'][time2])) + ' - less than 300, firstseen = 0')
-                    newData.append({'MAC':x['MAC'],'timeData':[{'firstSeen':x['timestamps'][time2]}],'visits':1, 'totalTime':(int(x['timestamps'][time1]) - int(x['timestamps'][time2]))})
-                    if time1 == 0:
-                        newData[arrayCount]['timeData'][timeCount]['lastSeen']=x['timestamps'][time1]
-                        timeCount=timeCount+1
-                    startFlag=1
-                    # startTimeTemp=time2
-                    firstSeen=1
-                    time2 = time1
-                    time1 = time1-1
-                elif int(x['timestamps'][time1]) - int(x['timestamps'][time2]) < _SESSION_TIME and startFlag == 0 and firstSeen == 1:
-                    # print(str(x['timestamps'][time1]) + ' - ' + str(x['timestamps'][time2]) + ' = ' + str(int(x['timestamps'][time1]) - int(x['timestamps'][time2])) + ' - less than 300, startflag=0')
-                    if time1 != 0:
-                        newData[arrayCount]['timeData'].append({'firstSeen':x['timestamps'][time2]})
-                        newData[arrayCount]['visits']=int(newData[arrayCount]['visits'])+1
-                        newData[arrayCount]['totalTime']=int(newData[arrayCount]['totalTime']) + (int(x['timestamps'][time1]) - int(x['timestamps'][time2]))
-                        startFlag=1
-                    time2 = time1
-                    time1 = time1-1
-                elif int(x['timestamps'][time1]) - int(x['timestamps'][time2]) < _SESSION_TIME and startFlag == 1 :
-                    # print(newData[arrayCount]['totalTime'])
-                    # print(str(x['timestamps'][time1]) + ' - ' + str(x['timestamps'][time2]) + ' = ' + str(int(x['timestamps'][time1]) - int(x['timestamps'][time2])) + ' less than 300, startflag=1')
-                    newData[arrayCount]['totalTime']=int(newData[arrayCount]['totalTime']) + (int(x['timestamps'][time1]) - int(x['timestamps'][time2]))
-                    if time1 == 0:
-                        newData[arrayCount]['timeData'][timeCount]['lastSeen']=x['timestamps'][time1]
-                        timeCount=timeCount+1
-                    time2 = time1
-                    time1 = time1-1
-                elif int(x['timestamps'][time1]) - int(x['timestamps'][time2]) >= _SESSION_TIME and startFlag == 1:
-                    # print(str(x['timestamps'][time1]) + ' - ' + str(x['timestamps'][time2]) + ' = ' + str(int(x['timestamps'][time1]) - int(x['timestamps'][time2])) +' greater than 300, startflag reset')
-                    newData[arrayCount]['timeData'][timeCount]['lastSeen']=x['timestamps'][time2]
-                    startFlag=0
-                    timeCount=timeCount+1
-                    time2 = time1
-                    time1 = time1-1
-                elif int(x['timestamps'][time1]) - int(x['timestamps'][time2]) >= _SESSION_TIME and startFlag == 0:
-                    # print(str(x['timestamps'][time1]) + ' - ' + str(x['timestamps'][time2]) + ' = ' + str(int(x['timestamps'][time1]) - int(x['timestamps'][time2])) + ' greater than 300, startflag=0')
-                    time2 = time1
-                    time1 = time1-1
-            if timeCount > 0:
-                arrayCount=arrayCount+1
-    return(newData)
 
-# filters out cmx data seen over a certain period of time
-def cmxFilterHours(data):
-    newData=getCMXHours(data)
-    returnData=[]
-    for x in newData:
-        if x['totalTime'] < _FILTER_TIME:
-            returnData.append(x)
-    return(returnData)
 
 # gets meraki MV video link
 def getMVLink(serial_number,timestamp):
@@ -191,58 +126,4 @@ def getDevices():
         return(resp.text)
     return('link error')
 
-# Aggregates amount of activity per hour throughout the day
-def computeCMXActivity(data):
-    newData = getCMXHours(data)
-    timesArray=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    for x in newData:
-        for y in x['timeData']:
-            inTime = int(datetime.fromtimestamp(float(y['firstSeen'])).strftime('%H'))
-            outTime = int(datetime.fromtimestamp(float(y['lastSeen'])).strftime('%H'))
-            # print('---'+inTime+'---'+outTime+'---'+count)
-            # count = count + 1
-            while inTime <= outTime:
-                timesArray[inTime] = timesArray[inTime] + 1
-                inTime = inTime+1
-    return(timesArray)
 
-# returns array of activity throughout the day of Mv activity
-def computeMVActivity(data):
-    timesArray=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    for x in data:
-        inTime = int(datetime.fromtimestamp(float(x['timeIn'])/1000).strftime('%H'))
-        outTime = int(datetime.fromtimestamp(float(x['timeOut'])/1000).strftime('%H'))
-        #
-        while inTime <= outTime:
-            timesArray[inTime] = timesArray[inTime] + int(x['count'])
-            inTime = inTime+1
-    return(timesArray)
-
-# correlate MV sense data with cmx data
-# when MV sense is triggered, check what MAC addresses have been found within certain time window and RSSI threshold
-# Time window for devices to be considered is slightly larger than the inTime and outTime of mv data
-def getCorrelation(cmxData,mvData):
-    data=[]
-    count=0
-    for x in mvData:
-        inTime = int(x['timeIn'])/1000
-        outTime = int(x['timeOut'])/1000
-        people = x['count']
-        flag=0
-        # restrict to those timestamp differences of more than one second
-        if outTime>inTime+1:
-            for y in cmxData:
-                for z in y['timestamps']:
-                    if int(z['ts']) >= (inTime-timeWindow) and int(z['ts']) <= (outTime+timeWindow) and int(z['rssi']) > rssiThreshold and flag == 0:
-                        data.append({'inTime':datetime.fromtimestamp(float(inTime)).strftime('%H:%M:%S'),'outTime':datetime.fromtimestamp(float(outTime)).strftime('%H:%M:%S'), 'count':people, 'devices':[{'MAC':y['MAC'],'time':datetime.fromtimestamp(float(z['ts'])).strftime('%H:%M:%S'),'rssi':z['rssi']}]})
-                        flag=1
-                    if int(z['ts']) >= (inTime-timeWindow) and int(z['ts']) <= (outTime+timeWindow) and int(z['rssi']) > rssiThreshold and flag == 1:
-                        update=1
-                        for d in data[count]['devices']:
-                            if y['MAC'] in d.values():
-                                update=0
-                        if update==1:
-                            data[count]['devices'].append({'MAC':y['MAC'],'time':datetime.fromtimestamp(float(outTime)).strftime('%H:%M:%S'),'rssi':z['rssi']})
-        if flag==1:
-            count+=1
-    return(data)
--- config.py
+++ config.py
@@ -1,16 +1,14 @@
 # this config file contains multiple variables utilized throughout the functionality of this code
 
-MERAKI_API_KEY = ""
-NETWORK_ID = ""
+MERAKI_API_KEY = "XXXXXXXXXXXXXXXXXXXXXXXX"
+NETWORK_ID = "YYYYYYYYYYYYYYYYYY"
 
 # ------------ Variables for Webex Teams bot notification -------------------------
 BOT_ACCESS_TOKEN = "xxxxx"
 
 # ------------Variables utilized in mvSense code-----------------------------------
-TEST_CAMERA_SERIAL="XXXXXXXX"
-
 MQTT_SERVER = "test.mosquitto.org"
-#MQTT_SERVER = "MQTT_IP_DOMAIN"
+
 MQTT_PORT = 1883
 
 # Array of valid cameras with MVSense API
@@ -25,25 +23,3 @@ MOTION_ALERT_PAUSE_TIME = 5
 # number of messages until action time out
 TIMEOUT = 20
 
-
-# --------Variables utilized in compute.py----------------------
-
-# number of seconds to filter out in cmxFilterHours()
-# will remove MAC addresses that have been seen for over this given amount of time
-_FILTER_TIME = 3600
-
-# number of seconds between timestamps in getCMXHours() which still constitutes a session
-# if a device is not seen for longer that this time threshold, the "session" will end
-# and this will be logged as a visit.
-# if the same device is seen again outside of this threshold, it will start a new visit session
-_SESSION_TIME = 300
-
-# utilized in correlation()
-# defines a leeway period before inTime and after outTime of mvSense data in which cmx data should still be considered, as the times may not exactly match up due to granularity differences
-timeWindow = 30
-
-# utilized in correlation()
-# defines threshold of signal strength that will be considered
-# the closer the device, the larger the rssiThreshold
-# tesing shows 45 and above is about 1 meter away from access point
-rssiThreshold = 45
